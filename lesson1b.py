eb13affe00fd9f3ba5708559bb3ea0b85beee41c
Your Name <you@example.com>
2026-02-04 13:22:04 +0300

    added lesson1b

---
+++ lesson1b.py
@@ -0,0 +1,15 @@
+# Python data types.
+# String- It is a series of characters that are in quotes
+name = "Joseph Kainembuga"
+print(type(name))
+
+# Integer- This indicates a whole number in python. For an onteger we don't use quotes
+age = 39
+print("The age of a person is: ", age)
+
+#This is a number with a decimal
+coordinates = 15.4524
+print("The coordinates are: ", coordinates)
+print(type(coordinates))
+
+# search and come up with an example of a complex data type
