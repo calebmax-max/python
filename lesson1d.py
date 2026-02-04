077493bb7e637c9fca89185a0f577973eae1fdd2
Your Name <you@example.com>
2026-02-04 20:04:22 +0300

    pyhton update

---
+++ lesson1d.py
@@ -0,0 +1,6 @@
+# Create a python program that is able to calculate a simple interest given the principle as 50000, rate as 5% and time as 8 years. SI = (p * R * t)/100
+principle = 50000
+rate = 5
+time = 8
+interest = (principle * rate * time) / 100
+print(interest)
--- test.py
+++ test.py
@@ -1,6 +1,5 @@
-print("Hello Caleb")
+
 
 #This is a comment
 """"This is another comment"""
 ''''This is another docstring'''
-
